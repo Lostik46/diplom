c4f7e4e9db1540b9ba8a6ccede5094d8fc006b45
Lostik46 <alekseyfedenko2006@gmail.com>
2025-06-21 18:47:28 +0300

    set

--- app/settings.py
+++ app/settings.py
@@ -108,7 +108,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',  # Папка static в корне проекта
 ]
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = '/media/' 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
